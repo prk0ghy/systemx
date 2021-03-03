689b7af5778e65e89de32a333673e242a0fe0ff2
Benjamin Vincent Schulenburg <bennyschulenburg@gmx.de>
2021-03-03 15:10:43 +0100

    A lot of Frontend styling, mostly regarding textpics/modals/license popups
    Co-authored-by: su-tobi <su-tobi@users.noreply.github.com>
    Co-authored-by: yo-hanna <yo-hanna@users.noreply.github.com>
    
    Former-commit-id: 0651b5ca3d35166dc076c44aed02a3f759122ea7

---
+++ front_end/themes/core/rights.js
@@ -0,0 +1,15 @@
+
+/* Don't pollute the global scope if avoidable */
+(() => {
+	function initRights() {
+		const licenseButtons = document.querySelectorAll("details.license > summary");
+		licenseButtons.forEach((license) => {
+			license.parentElement.setAttribute("open","");
+			license.addEventListener('click',(e) => {
+				e.preventDefault();
+				license.parentElement.classList.toggle("active");
+			});
+		});
+	}
+	setTimeout(initRights, 0);
+})();
