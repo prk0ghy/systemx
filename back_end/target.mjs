8cdae12d3954b6d885d252ec4128c2223cceef6e
Kai Riemann <kai@riemann.dev>
2021-03-03 08:39:25 +0100

    Used common code path for directory creation in `target.mjs`
    Former-commit-id: 2d175ce416d51cdd0116789074f2e320c7610248

--- back_end/target.mjs
+++ back_end/target.mjs
@@ -5,6 +5,15 @@ import fs from "fs";
 
 const fsp = fs.promises;
 
+async function mkdirp(path) {
+	try {
+		await fs.mkdirSync(path);
+	}
+	catch (error) {
+		console.error("Directory creation has failed.", error);
+	}
+}
+
 function fileExtension(filename) {
 	return filename.substring(filename.lastIndexOf(".") + 1, filename.length) || filename;
 }
@@ -34,17 +43,13 @@ async function renderFile(source, destination, targetName) {
 }
 
 async function copyDir(source, destination, targetName) {
-	try {
-		await fsp.mkdir(destination);
-	}
-	catch (e) {/* Fails if dir already exists */}
+	await mkdirp(destination);
 	const fileNames = await fsp.readdir(source);
 	const promises = [];
-
 	for (const fileName of fileNames) {
 		try {
-			const s = await fsp.stat(source + "/" + fileName);
-			if (s.isFile()) {
+			const stat = await fsp.stat(source + "/" + fileName);
+			if (stat.isFile()) {
 				const ext = fileExtension(source + "/" + fileName).toLowerCase();
 				if (ext === "html") {
 					promises.push(renderFile(source + "/" + fileName, destination + "/" + fileName, targetName));
@@ -53,11 +58,11 @@ async function copyDir(source, destination, targetName) {
 					promises.push(fsp.copyFile(source + "/" + fileName, destination + "/" + fileName));
 				}
 			}
-			else if (s.isDirectory()) {
+			else if (stat.isDirectory()) {
 				promises.push(copyDir(source + "/" + fileName, destination + "/" + fileName, targetName));
 			} // Ignore everything else
 		}
-		catch (e) {/* If we can't stat then just skip */}
+		catch {/* If we can't stat then just skip */}
 	}
 	return Promise.all(promises);
 }
@@ -68,9 +73,9 @@ async function copyAssets(destination) {
 }
 
 export async function build(targetName) {
-	try { fs.mkdirSync("web");}                         catch (e) {/* Fails if dir already exists */}
-	try { fs.mkdirSync("web/" + targetName); }          catch (e) {/* Fails if dir already exists */}
-	try { fs.mkdirSync("web/" + targetName + "/res"); } catch (e) {/* Fails if dir already exists */}
+	mkdirp("web");
+	mkdirp("web/" + targetName);
+	mkdirp("web/" + targetName + "/res");
 	/* If any of those fail for other reasons, then we will catch that later when we
 	 * actually try and write out some files.
 	 */
