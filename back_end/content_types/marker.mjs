a9e1f4e7c891d19676a982a2d29cc0d6ce1b2a62
Benjamin Vincent Schulenburg <bennyschulenburg@gmx.de>
2021-03-18 13:06:23 +0100

    Rewrote the marker code without cheerio, yielding ~2x speed up.
    Former-commit-id: fe23e7f1e7b0ccf8261161c22af72d910e60ff32

--- back_end/content_types/marker.mjs
+++ back_end/content_types/marker.mjs
@@ -1,16 +1,13 @@
-import cheerio from "cheerio";
-
 export function getRenderer() {
 	return [];
 }
 
 export function fill(html) {
-	const $ = cheerio.load(html);
-	const getNumber = index => String(index + 1);
-
-	$(".marker").text(getNumber).attr("id", getNumber);
-
-	return $.html();
+	let i = 0;
+	return String(html).replace(/<a class="marker"><\/a>/g, () => {
+		++i;
+		return `<a class="marker" id="${i}">${i}</a>`;
+	});
 }
 
 export async function render({ isNumbered }) {
