3c90c6501d2151e34d0ced4b17e9644d52e99488
Kai Riemann <kai@riemann.dev>
2021-04-12 22:18:51 +0200

    Externalized `TextAndImage` methods
    The previous way would define them on every call to `render`.
    It's more performant to persist their definitions.
    
    
    Former-commit-id: f3ec970b962ed215a81b59de3e99a51b6ab8cbbd

--- back_end/types/content/TextAndImage.mjs
+++ back_end/types/content/TextAndImage.mjs
@@ -1,4 +1,24 @@
 export default {
+	getFigureWidth(width) {
+		switch (width) {
+		case "38.2":
+			return "30";
+		case "61.8":
+			return "50";
+		case "100":
+		default:
+			return "100";
+		}
+	},
+	getFigurePosition(position) {
+		switch (position) {
+		case "linksImText":
+		default:
+			return "left";
+		case "rechtsImText":
+			return "right";
+		}
+	},
 	queries: new Map([
 		["inhaltsbausteine_textMitOhneBild_BlockType", cms => `
 			__typename
@@ -33,51 +53,45 @@ export default {
 			Marker
 		}
 	}) {
-		function mapWidth(width){
-			switch(width){
-			case "38.2":
-				return "30";
-			case "61.8":
-				return "50";
-			case "100":
-			default:
-				return "100";
-			}
-		}
-
-		function mapPosition(position){
-			switch(position){
-			case "linksImText":
-			default:
-				return "left";
-			case "rechtsImText":
-				return "right";
-			}
-		}
-
-		function renderImageFigure(singleImage, imageWidth, imagePosition, displayInOneLine){
-			const figcaption = singleImage.caption ? `<figcaption>${singleImage.caption}</figcaption>` : "";
-			return `
-				<figure figure-position="${imagePosition}" figure-type="picture" figure-width="${imageWidth}" one-line="${displayInOneLine}">
-					${Image.render({asset: singleImage?.image?.[0]})}
-					${figcaption}
-				</figure>
-			`;
-		}
-		const mappedImageWidth = mapWidth(imageWidth);
-		const mappedImagePosition = mapPosition(imagePosition);
-		const imageHTML = images.map((i) => renderImageFigure(i, mappedImageWidth, mappedImagePosition, displayInOneLine)).join("");
-		const galleryIntroductionHTML = galleryIntroductionText ? `<gallery-introduction-text>${galleryIntroductionText}</gallery-introduction-text>` : "";
-
+		const mappedImageWidth = this.getFigureWidth(imageWidth);
+		const mappedImagePosition = this.getFigurePosition(imagePosition);
+		const figureHTML = images
+			.map(image => this.renderFigure({
+				caption: image.caption,
+				displayInOneLine,
+				imageHTML: Image.render({ asset: image?.image?.[0] }),
+				width: mappedImageWidth,
+				position: mappedImagePosition
+			})).join("");
+		const galleryIntroductionHTML = galleryIntroductionText
+			? `<gallery-introduction-text>${galleryIntroductionText}</gallery-introduction-text>`
+			: "";
 		return `
 			<section content-type="text-and-image">
 				<inner-content>
 					${Marker.render({ isNumbered })}
-					${imageHTML}
+					${figureHTML}
 					${galleryIntroductionHTML}
-					${text ? text : ""}
+					${text ?? ""}
 				</inner-content>
 			</section>
+		`;
+	},
+	renderFigure({
+		caption,
+		displayInOneLine,
+		imageHTML,
+		position,
+		width
+	}) {
+		const captionHTML = caption
+			? `<figcaption>${caption}</figcaption>`
+			: "";
+		return `
+			<figure figure-position="${position}" figure-type="picture" figure-width="${width}" one-line="${displayInOneLine}">
+				${imageHTML}
+				${captionHTML}
+			</figure>
 		`;
 	}
 };
