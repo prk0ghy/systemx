e4fc741b584f46fe8565363153dd1153b05b76ea
Benjamin Vincent Schulenburg <bennyschulenburg@gmx.de>
2021-04-12 19:14:08 +0200

    FE
    Former-commit-id: 28b7ca375c86e31415488dae42f8483f2b0a6714

---
+++ back_end/types/content/Glossar.mjs
@@ -0,0 +1,57 @@
+export default {
+	queries: new Map([
+		["glossar_glossar_Entry", ({
+			fragments,
+			types
+		}) => `
+		elements: inhaltsbausteine {
+			__typename
+			...on inhaltsbausteine_audioDatei_BlockType {
+				${types.inhaltsbausteine_audioDatei_BlockType}
+			}
+			...on inhaltsbausteine_galerie_BlockType {
+				${types.inhaltsbausteine_galerie_BlockType}
+			}
+			...on inhaltsbausteine_textMitOhneBild_BlockType {
+				${types.inhaltsbausteine_textMitOhneBild_BlockType}
+			}
+			...on inhaltsbausteine_ueberschrift_BlockType {
+				${types.inhaltsbausteine_ueberschrift_BlockType}
+			}
+			...on inhaltsbausteine_videoDatei_BlockType {
+				${types.inhaltsbausteine_videoDatei_BlockType}
+			}
+		}
+		`]
+	]),
+	async render({
+		id,
+		title
+	}, {
+		cms,
+		query,
+		render,
+		type
+	}) {
+		/*
+		const content = await query(() => `
+			entry(id: ${id}) {
+				...on ${type} {
+					${this.queries.get(type)(cms)}
+				}
+			}
+		`);
+		const children = await Promise.all(content.entry.elements.map(element => render(element)));
+		*/
+		return '';
+
+		return `
+			<section content-type="headline">
+				<inner-content>
+					<h1 class="content-title inner-content">${title}</h1>
+				</inner-content>
+			</section>
+			${children.join("")}
+		`;
+	}
+};
