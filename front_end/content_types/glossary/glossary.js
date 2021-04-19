e29647d219dea7c27082fe93c1f1f79930107f40
Benjamin Vincent Schulenburg <benjamin.schulenburg@institut-fuer-digitales-lernen.de>
2021-04-19 12:39:14 +0200

    Renamed glossar -> glossary within the FE code.
    Former-commit-id: da25e21d8ee7337b51810078bf7ec0fa5a81d722

---
+++ front_end/content_types/glossary/glossary.js
@@ -0,0 +1,55 @@
+/* globals showModal */
+
+(() => {
+	function getLinkPath(href){
+		return "/"+(String(href).split("/").slice(3).join("/"));
+	}
+
+	async function fetchGlossaryEntry(rawHref){
+		try {
+			const getGlossar = await fetch(rawHref);
+			if(getGlossar.ok){
+				const parser       = new DOMParser();
+				const htmlDocument = parser.parseFromString(await getGlossar.text(), "text/html");
+				const main         = htmlDocument.documentElement.querySelector("main");
+				if(main !== null){
+					return main.innerHTML;
+				}
+			}
+		} catch(e){ /* Doesn't matter */ }
+		return `<section content-type="error"><inner-content><h3>Der Glossareintrag konnte nicht geladen werden</h3></inner-content></section>`
+	}
+
+	async function loadGlossaryEntry(rawHref){
+		const content = await fetchGlossaryEntry(rawHref);
+		const entry = document.createElement("GLOSSARY-ENTRY");
+		entry.setAttribute("entry-href",getLinkPath(rawHref));
+		entry.innerHTML = `<glossary-entry-content>${content}</glossary-entry-content>`;
+		document.querySelector("main").appendChild(entry);
+		showModal(entry);
+	}
+
+	function initGlossaryLink(a){
+		a.classList.add("glossary-link");
+		a.addEventListener("click",async (e) => {
+			e.preventDefault();
+			const href  = getLinkPath(a.href);
+			const entry = document.querySelector(`glossary-entry[entry-href="${href}"]`);
+			if(entry !== null){ // Just show the entry if it is already loaded, otherwise load it
+				showModal(entry);
+			}else{
+				loadGlossaryEntry(a.href);
+			}
+		});
+	}
+
+	function initGlossary() {
+		for(const link of document.querySelectorAll("a")){
+			const hrefArr = link.href.split("/");
+			if(hrefArr.length < 5){continue;}
+			if(hrefArr[3] !== "glossar"){continue;}
+			initGlossaryLink(link);
+		}
+	}
+	setTimeout(initGlossary,0);
+})();
