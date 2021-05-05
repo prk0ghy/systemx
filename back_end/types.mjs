872bac13f701aa3f3bc3ab4e036fd7b73e17994a
Kai Riemann <kai@riemann.dev>
2021-05-05 13:06:26 +0200

    Split up content types into `entry` and `block` types
    This commit splits up our content types into two categories:
    
    (1) `entry` types, meaning types that can appear on the outermost
        level. In CraftCMS, entry types will result in an HTML page.
    
    (2) `block` types, meaning types that can appear somewhere within an
        `entry` type.
    
    
    Former-commit-id: 33b8efe10cbca558200927f7e93792b6ba8337ef

--- back_end/types.mjs
+++ back_end/types.mjs
@@ -2,13 +2,17 @@ import fs from "fs";
 import getPackageDirectory from "pkg-dir";
 import path from "path";
 import URL from "url";
-const loadTypes = async directoryName => {
-	const types = new Map();
-	const root = await getPackageDirectory();
-	const directory = path.join(root, "back_end", "types", directoryName);
-	const fileList = await fs.promises.readdir(directory);
-	await Promise.all(fileList.map(async fileName => {
+const recurseDirectory = async (directory, typeMap) => {
+	const fileList = await fs.promises.readdir(directory, {
+		withFileTypes: true
+	});
+	await Promise.all(fileList.map(async directoryEntry => {
 		const moduleExtension = ".mjs";
+		const { name: fileName } = directoryEntry;
+		if (directoryEntry.isDirectory()) {
+			const subdirectory = path.join(directory, directoryEntry.name);
+			await recurseDirectory(subdirectory, typeMap);
+		}
 		if (!fileName.endsWith(moduleExtension)) {
 			return;
 		}
@@ -16,13 +20,19 @@ const loadTypes = async directoryName => {
 		const moduleURL = URL.pathToFileURL(path.join(directory, fileName));
 		try {
 			const module = await import(moduleURL);
-			types.set(moduleName, module);
+			typeMap.set(moduleName, module);
 		}
 		catch (error) {
 			console.error(`Couldn't load type module at ${moduleURL}`);
 			console.error(error);
 		}
 	}));
+};
+const loadTypes = async directoryName => {
+	const types = new Map();
+	const root = await getPackageDirectory();
+	const directory = path.join(root, "back_end", "types", directoryName);
+	await recurseDirectory(directory, types);
 	return types;
 };
 export const loadContentTypes = () => loadTypes("content");
