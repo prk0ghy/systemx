fafd1c5f05615e6ca58ccaae2c423fcfde86b2e8
Kai Riemann <kai@riemann.dev>
2021-05-07 17:09:43 +0200

    Mapped `blau` to `blue` in `Box`
    Former-commit-id: 930ba3c334fae064ab34ba38801d13e0c36b787c

--- back_end/types/content/block/Box.mjs
+++ back_end/types/content/block/Box.mjs
@@ -13,8 +13,7 @@ export default {
 			? "gray"
 			: colorClassName
 				.replace(/^box-/, "")
-				/* German names are considered deprecated */
-				.replace("blau", "gray")
+				.replace("blau", "blue")
 				.replace("grey", "gray");
 	},
 	queries: new Map([
