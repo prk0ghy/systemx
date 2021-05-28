d9eed13e2bde4605e6fb32b3606511fbfee17f3e
Benjamin Vincent Schulenburg <bennyschulenburg@gmx.de>
2021-05-28 18:41:23 +0200

    Added Cheerio and used it to test the Download type a lot more thoroughly
    Former-commit-id: a13a64adc711b48112fc0ee2bfa4a8d7e7f9f2fa

--- back_end/test/Download.test.mjs
+++ back_end/test/Download.test.mjs
@@ -1,4 +1,5 @@
 import { makeMockRenderer } from "../renderer.mjs";
+import cheerio from "cheerio";
 it("renders `inhaltsbausteine_download_BlockType`", async () => {
 	const render = await makeMockRenderer();
 	const html = await render({
@@ -10,4 +11,19 @@ it("renders `inhaltsbausteine_download_BlockType`", async () => {
 		__typename: "inhaltsbausteine_download_BlockType"
 	});
 	expect(html.includes("Hello, world")).toBe(true);
+	const dom = cheerio.load(html);
+	const section = dom("section");
+	expect(section.length).toBe(1);
+	expect(section.attr("content-type")).toBe("download");
+	expect(section.children("inner-content").length).toBe(1);
+	expect(section.children().length).toBe(1);
+	expect(dom("download-wrap").length).toBe(1);
+	expect(dom("download-wrap download-text").length).toBe(1);
+	expect(dom("download-wrap download-icon").length).toBe(1);
+	expect(dom(".marker").length).toBe(1);
+	expect(dom(".marker").text()).toBe("");
+	expect(dom("download-wrap a").length).toBe(1);
+	expect(dom("download-wrap a").attr("href")).toBe("https://example.invalid");
+	expect(dom("download-wrap a").attr("target")).toBe("_blank");
+	expect(dom("download-text").text().trim()).toBe("Hello, world!");
 });
