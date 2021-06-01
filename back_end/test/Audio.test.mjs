2626710e3718777c8f092695c46342f7f151097f
Benjamin Vincent Schulenburg <bennyschulenburg@gmx.de>
2021-06-01 13:07:09 +0200

    Added a new Test
    Former-commit-id: 977826052e90676bbb591eef368124a239dde189

---
+++ back_end/test/Audio.test.mjs
@@ -0,0 +1,32 @@
+import { makeMockRenderer } from "../renderer.mjs";
+import cheerio from "cheerio";
+it("renders `inhaltsbausteine_audioDatei_BlockType`", async () => {
+	const render = await makeMockRenderer();
+	const html = await render({
+		audioFiles: [{"url":"https://test.test/audio.mp3"}],
+		caption: "Testcaption",
+		id: "42",
+		isNumbered: true,
+		posters: [{"url":"https://test.test/poster.jpg"}],
+		__typename: "inhaltsbausteine_audioDatei_BlockType"
+	});
+	const $ = cheerio.load(html);
+	const section = $("section");
+	expect(section.length).toBe(1);
+	expect(section.attr("content-type")).toBe("audio");
+	expect(section.children("inner-content").length).toBe(1);
+	expect(section.children().length).toBe(1);
+	expect($(".marker").length).toBe(1);
+	expect($(".marker").text()).toBe("");
+	const figure = $(`figure[figure-type="audio"]`);
+	expect(figure.length).toBe(1);
+	const audio = figure.find("audio");
+	expect(audio.length).toBe(1);
+	expect(audio.attr("controls")).toBe("controls");
+	expect(audio.attr("src").length).toBeGreaterThan(4);
+	expect(audio.attr("src")).toBe("https://test.test/audio.mp3");
+	const poster = $(`img`);
+	expect(poster.length).toBe(1);
+	expect(poster.attr('src')).toBe("https://test.test/poster.jpg");
+	expect(figure.html().includes("Testcaption")).toBe(true);
+});
