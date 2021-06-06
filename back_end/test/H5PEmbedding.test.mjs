9aadbb2b6b858874bb48205c92518582e33413c6
Benjamin Vincent Schulenburg <bennyschulenburg@gmx.de>
2021-06-07 00:12:23 +0200

    Added a simple test for embeddings
    Former-commit-id: 5430108e726766b1b2a7c22a0e727b1a3ca24120

---
+++ back_end/test/H5PEmbedding.test.mjs
@@ -0,0 +1,27 @@
+import cheerio from "cheerio";
+import { makeMockRenderer } from "../renderer.mjs";
+it("renders `inhaltsbausteine_h5p_BlockType`", async () => {
+	const render = await makeMockRenderer();
+	const html = await render({
+		caption: "H5P Caption",
+		id: "23",
+		isNumbered: true,
+		html: `<iframe src="https://some.random.iframe/"/>`,
+		__typename: "inhaltsbausteine_h5p_BlockType"
+	});
+	const $ = cheerio.load(html);
+	const section = $("section");
+	expect(section.length).toBe(1);
+	expect(section.attr("content-type")).toBe("embedding");
+	expect(section.children("inner-content").length).toBe(1);
+	expect(section.children().length).toBe(1);
+	expect($(".marker").length).toBe(1);
+	expect($(".marker").text()).toBe("");
+	const figure = $(`figure[figure-type="embedding"]`);
+	expect(figure.length).toBe(1);
+	const iframe = figure.find("iframe");
+	expect(iframe.length).toBe(1);
+	expect(iframe.attr("src").length).toBeGreaterThan(4);
+	expect(iframe.attr("src")).toBe("https://some.random.iframe/");
+	expect(figure.html().includes("H5P Caption")).toBe(true);
+});
