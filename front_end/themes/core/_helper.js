94e4bc66e1a7ac7f8a6375e39fa7148346e0a1c7
Benjamin Vincent Schulenburg <bennyschulenburg@gmx.de>
2021-06-10 19:36:59 +0200

    Fixed scroll offset on leaving fullscreen mode
    This only occured if the element that got fullscreen focus is at the
    bottom end of the document, thereby reducing the overall document height
    when removing it from the document flow due to entering fullscreen mode
    
    
    Former-commit-id: 6a43e1d0948956128cd95103fa4fa620b805fe84

--- front_end/themes/core/_helper.js
+++ front_end/themes/core/_helper.js
@@ -17,13 +17,32 @@ function hideElementContentHandler(ele){
 	}
 }
 
+/* Important to do this here and not in the closeFullscreen
+ * procedure because a user might exit fullscreen mode by
+ * pressing ESC or something similar.
+ */
+document.addEventListener("fullscreenchange", () => {
+	if( window.innerHeight === screen.height){return;}
+	document.documentElement.style.height = "";
+});
+
+/* The document height needs to be set to a fixed value because as soon
+ * as we enter fullscreen mode, the element gets removed from the
+ * text flow, reducing the overall document height. Now as soon as we
+ * return to the normal mode the browsers scroll changes because it is
+ * larger than the overall document height. By setting it on entering
+ * fullscreen (and removing the value later) we work around that issue.
+ */
 function openFullscreen(element) {
+	document.documentElement.style.height = (document.documentElement.scrollHeight|0)+"px";
 	if (element.requestFullscreen) {
 		element.requestFullscreen();
 	} else if (element.webkitRequestFullscreen) {
 		element.webkitRequestFullscreen();
 	} else if (element.msRequestFullscreen) {
 		element.msRequestFullscreen();
+	} else {
+		return;
 	}
 }
 
@@ -35,5 +54,7 @@ function closeFullscreen() {
 		document.webkitExitFullscreen();
 	} else if (document.msExitFullscreen) {
 		document.msExitFullscreen();
+	} else {
+		return;
 	}
 }
