489e1611a96b843984b5ce47c58e1a88f6dca66a
Benjamin Vincent Schulenburg <bennyschulenburg@gmx.de>
2021-06-17 16:05:57 +0200

    Moved everything ssg related into the new `ssg/` directory
    Former-commit-id: e36e533b0e2535774455acb9fe304b7266d7de82

--- ssg/back_end/index.mjs
+++ ssg/back_end/index.mjs
@@ -2,6 +2,7 @@ import * as target  from "./target.mjs";
 import open from "open";
 import {currentTarget, default as options} from "./options.mjs";
 import serve from "./server.mjs";
+
 (async () => {
 	await target.build(currentTarget);
 	if (options.startServer) {
