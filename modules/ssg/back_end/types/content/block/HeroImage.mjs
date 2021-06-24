7a203f5829d95379dc5bd0449e9c2a0321e1140d
Benjamin Vincent Schulenburg <bennyschulenburg@gmx.de>
2021-06-24 16:54:55 +0200

    HeroVideos don't loop anymore
    Former-commit-id: 20d1ee73765792c870091806d837dca5c6e0ae56

--- modules/ssg/back_end/types/content/block/HeroImage.mjs
+++ modules/ssg/back_end/types/content/block/HeroImage.mjs
@@ -55,7 +55,7 @@ export default {
 		});
 		const isVideo = image.mimeType.substr(0,5) === "video";
 		const mediaHTML = isVideo
-			? await Video.render({asset: image, controls: false, muted: true, loop: true, autoplay: true})
+			? await Video.render({asset: image, controls: false, muted: true, loop: false, autoplay: true})
 			: await Image.render({asset: image});
 		const captionHTML = caption
 			? `<figcaption>${caption}</figcaption>`
