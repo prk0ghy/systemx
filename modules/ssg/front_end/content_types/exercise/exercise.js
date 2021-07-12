f165f8e3047694b7120b276be19f9b3fb0892b6c
Tobias Pätzold <tobias.paetzold@institut-fuer-digitales-lernen.de>
2021-07-12 15:05:23 +0200

    quill styling Co-authored-by: RainerZarth <RainerZarth@users.noreply.github.com>
    Former-commit-id: 81e5c839f4d5dbc058a10821ab8197fe7ff69f19

--- modules/ssg/front_end/content_types/exercise/exercise.js
+++ modules/ssg/front_end/content_types/exercise/exercise.js
@@ -1,5 +1,6 @@
 /* globals Quill,getFirstParentSection */
 
+
 (() => {
 	const initExerciseEditors = () => {
 		const textareas = document.querySelectorAll(".exercise-text");
@@ -19,7 +20,6 @@
 			icons.background = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="12 11 26.7 26"><defs><clipPath id="prefix__clip-path" transform="translate(12.25 11.3)"><path class="prefix__cls-fill-none" fill="none" d="M0 0h26.23v26.23H0z"/></clipPath><style>.prefix__cls-1{fill:#444}</style></defs><g clip-path="url(#prefix__clip-path)"><path class="prefix__cls-1" d="M-.007 25.96L25.958-.003l.707.707L.7 26.668zM3.438 29.4L29.403 3.437l.707.707L4.145 30.108zM6.88 32.84l.7.71 9.8-9.8 1.48-2.9L6.88 32.84zM33.55 7.58l-.71-.7-5.37 5.37.48.93 5.6-5.6zM19.5 28.37l-.12.27-.02.02.14-.29zM13.73 32.86l-3.42 3.42.71.71 3.65-3.65-.86-.44-.08-.04zM23.15 23.44l-.47.93h.96l2.92-2.92-.48-.94-2.93 2.93zM36.99 11.02l-.71-.71-6.48 6.48.48.94 6.71-6.71zM40.42 14.46l-.7-.71-7.59 7.59.48.93 7.81-7.81zM13.75 39.72l.71.7 11.05-11.05H24.1L13.75 39.72zM17.19 43.16l.71.7 13.32-13.32-.48-.93-13.55 13.55zM43.86 17.9l-.7-.71-8.7 8.7.48.93 8.92-8.92zM47.3 21.33l-.71-.7-9.8 9.8.48.94L47.3 21.33zM34.26 34.27l-.9.46-.29-.58-12.44 12.44.7.71 13.15-13.15-.22.12zM24.073 50.03l25.965-25.964.707.707L24.78 50.738zM29.7 25.37h-.18l.12-.12.06.12z"/><path class="prefix__cls-1" d="M36.04 31.18l-1.85-3.62-.48-.94-1.85-3.6-.48-.94-1.85-3.61-.48-.93-1.8-3.61-.48-.95-.2-.38h-2.32l-4.57 8.84L14.25 32l.21.12.94.48.18.09 1.34.68.33-.68 2.07-4 .15-.29h11.78l.26.48.48.94 1.48 2.9.35.68h.1l1.24-.64 1.33-.69zm-15-5.81l4.33-8.44 1.46 2.84.48.92 1.84 3.61.48.94.06.12z"/></g></svg>';
 			icons.list.ordered = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23.83 22.75"><path fill="#444" d="M5.91 1.35h17.92v3H5.91zM5.91 9.9h17.92v3H5.91zM5.91 18.4h17.92v3H5.91zM2.82.57v4.56a.53.53 0 01-.58.57.53.53 0 01-.57-.57V1.78L1.42 2a.6.6 0 01-.34.18A.51.51 0 01.66 2a.56.56 0 01-.17-.41.6.6 0 01.19-.39l1-1a.78.78 0 01.61-.2.51.51 0 01.53.57zM4.19 13.68a.53.53 0 01-.57.57H.75a.53.53 0 01-.58-.57 2.29 2.29 0 01.19-1A2.11 2.11 0 01.83 12a3.55 3.55 0 01.58-.46l.59-.31.71-.41a1.67 1.67 0 00.36-.3.49.49 0 00.09-.3c0-.33-.07-.42-.09-.44s-.11-.08-.44-.08h-.87a1 1 0 00-.41.06.26.26 0 00-.09.18.71.71 0 01-.17.36.54.54 0 01-.42.16.57.57 0 01-.42-.16.51.51 0 01-.14-.45 1.47 1.47 0 01.52-1 1.85 1.85 0 011.13-.32h.82A1.7 1.7 0 013.83 9a1.7 1.7 0 01.42 1.25 1.54 1.54 0 01-.27.92 2.59 2.59 0 01-.66.61l-.8.48-.52.31a2.07 2.07 0 00-.41.36.71.71 0 00-.12.18h2.2a.53.53 0 01.52.57zM4.36 21a1.78 1.78 0 01-.44 1.3 1.79 1.79 0 01-1.31.44h-.87a1.83 1.83 0 01-1.19-.34 1.47 1.47 0 01-.54-1 .55.55 0 01.13-.46.62.62 0 01.43-.16.55.55 0 01.58.54.34.34 0 00.12.23 1.15 1.15 0 00.47.07h.87a.83.83 0 00.5-.1s.1-.14.1-.49-.07-.46-.1-.49a.83.83 0 00-.5-.1H2a.58.58 0 010-1.15h.9c.07 0 .09 0 .11-.07a1.19 1.19 0 00.08-.48c0-.34-.07-.43-.09-.44s-.11-.09-.44-.09h-.74a1 1 0 00-.41.06.26.26 0 00-.1.19.56.56 0 01-.58.54.58.58 0 01-.43-.17.51.51 0 01-.12-.46 1.41 1.41 0 01.52-1 1.8 1.8 0 011.12-.31h.74a1.7 1.7 0 011.25.42 1.7 1.7 0 01.42 1.25 1.59 1.59 0 01-.28 1 1.15 1.15 0 01.26.41 2.33 2.33 0 01.15.86z"/></svg>';
 			icons.list.bullet = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22.88 20.05"><path fill="#444" d="M4.96 0h17.92v3H4.96zM4.96 8.56h17.92v3H4.96zM4.96 17.05h17.92v3H4.96z"/><circle cx="1.35" cy="1.5" r="1.35" fill="#444"/><circle cx="1.35" cy="10.06" r="1.35" fill="#444"/><circle cx="1.35" cy="18.61" r="1.35" fill="#444"/></svg>';
-			console.log(icons);
 			const editor = new Quill(fancyEditor, {
 				modules: {
 					toolbar: [
@@ -50,7 +50,44 @@
 					const contentJSON = JSON.stringify(editor.getContents());
 					localStorage.setItem(localStorageKey, contentJSON);
 				}, 200); // Only write to localStorage after 200ms of inactivity
+				//change underline of QL Picker with actual color
+				const pickerLabel = document.querySelector(".ql-color-picker .ql-picker-label");
+				const pickerSvg = pickerLabel.querySelector("svg path:last-child");
+				const colorValue = pickerLabel.dataset.value;
+				if (colorValue !== null) {
+					pickerSvg.style.fill = pickerLabel.dataset.value;
+				}
 			});
+
+			const colorPalletteSelector = document.querySelectorAll("span.ql-picker-item");
+
+			for (let i = 0; i < colorPalletteSelector.length; i++) {
+				colorPalletteSelector[i].addEventListener("click", () => {
+					//change underline of QL Picker with actual color
+					const pickerColor = document.querySelector(".ql-color .ql-picker-label");
+					const pickerSvg = pickerColor.querySelector("svg path:last-child");
+					const colorValue = pickerColor.dataset.value;
+
+					if (colorValue !== undefined) {
+						pickerSvg.style.fill = pickerColor.dataset.value;
+					}
+					else {
+						pickerSvg.style.fill = "#000000";
+					}
+
+					//change Background of QL Picker with actual color
+					const pickerBackground = document.querySelector(".ql-background .ql-picker-label");
+					const pickerSvgBg = pickerBackground.querySelector("svg");
+					const colorValueBg = pickerBackground.dataset.value;
+
+					if (colorValueBg !== undefined) {
+						pickerSvgBg.style.backgroundColor = pickerBackground.dataset.value;
+					}
+					else {
+						pickerSvgBg.style.backgroundColor = "transparent";
+					}
+				});
+			}
 
 			const loadedContentJSON = localStorage.getItem(localStorageKey);
 			if (loadedContentJSON !== null) {
