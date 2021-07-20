4f65d93450544590094c2ce6cef1fbbc7e120a33
Kai Riemann <kai@riemann.dev>
2021-07-20 13:31:11 +0200

    Implemented PayPal fee computation, added registration flow in checkout
    Former-commit-id: b858efc11542727fd5bf847e96d2719b2e47f3f8

--- modules/shop/frontend.mjs
+++ modules/shop/frontend.mjs
@@ -1,186 +1,200 @@
 import * as configuration from "./configuration.mjs";
 import * as feuser from "./feuser.mjs";
-import * as fesession from "./fesession.mjs";
+import * as frontEndSession from "./fesession.mjs";
 import * as pwreset from "./pwreset.mjs";
 import * as shop from "./shop.mjs";
 import * as template from "./template.mjs";
-
 const resetRequests = {};
-
 const addDeletionRequest = async id => {
 	const user = await feuser.getByID(id);
-	if(user === undefined){return false;}
+	if (!user) {
+		return false;
+	}
 	const hash = configuration.makeid(32);
 	resetRequests[hash] = user.ID|0;
-	const delLink = configuration.absoluteUrl('/userDelete/'+hash);
-	const mailText = 'Um Ihren Account unwiederruflich zu löschen, besuchen Sie bitte folgenden Link: '+delLink;
-	//let mailHtml = '<h3>Passwort Löschen</h3><p>Um Ihren Account unwiederruflich zu löschen, besuchen Sie bitte folgenden Link: <a href="'+delLink+'">'+delLink+'</a></p>';
-	//mail.send("proexly@proexly-test.de",user.email,"Passwort Zuruecksetzen",mailText,mailHtml);
+	const deletionURL = configuration.absoluteUrl("/userDelete/" + hash);
+	const mailText = "Um Ihr Konto unwiederruflich zu löschen, besuchen Sie bitte folgenden Link: " + deletionURL;
+	//let mailHtml = "<h3>Passwort Löschen</h3><p>Um Ihr Konto unwiederruflich zu löschen, besuchen Sie bitte folgenden Link: <a href=""+deletionURL+"">"+deletionURL+"</a></p>";
+	//mail.send("proexly@proexly-test.de", user.email, "Passwort Zuruecksetzen", mailText, mailHtml);
 	console.log(mailText);
 	return true;
 };
-
-export const reqLogin = async ctx => {
-	const arr = {};
-	const ses = fesession.get(ctx);
-	arr.title  = 'Login';
-	arr.loggedIn = false;
-	arr.status = 'Bitte gib deine Zugangsdaten ein.';
-	arr.pwresethref = configuration.prefixUrl('/pwreset');
-	arr.loginhref = configuration.absoluteUrl('/login');
-	arr.logouthref = configuration.absoluteUrl('/logout');
-	if(!ses && (ctx.method === 'POST') && (ctx.request.body !== undefined) && (ctx.request.body.username !== undefined)){
-		if(await fesession.checkPassword(ctx)){
-			ctx.redirect(ctx.request.body.redirect);
+export const reqLogin = async context => {
+	const values = {};
+	const session = frontEndSession.get(context);
+	values.title = "Login";
+	values.loggedIn = false;
+	values.status = "Bitte gib deine Zugangsdaten ein.";
+	values.pwresethref = configuration.prefixUrl("/pwreset");
+	values.loginhref = configuration.absoluteUrl("/login");
+	values.logouthref = configuration.absoluteUrl("/logout");
+	if (!session && context.method === "POST" && context.request.body && context.request.body.username) {
+		if (await frontEndSession.checkPassword(context)) {
+			context.redirect(context.request.body.redirect);
 			return;
-		}else {
-			arr.status = 'Passwort ungueltig, bitte versuche es erneut!';
 		}
-	}else if(ses){
-		arr.status = 'Du bist bereits eingeloggt';
-		arr.loggedIn = true;
+		else {
+			values.status = "Passwort ungültig, bitte versuche es erneut!";
+		}
 	}
-	ctx.body = await template.renderPage('login',arr,false,ctx);
+	else if (session) {
+		values.status = "Du bist bereits angemeldet";
+		values.loggedIn = true;
+	}
+	context.body = await template.renderPage("login", values, false, context);
 };
-
-export const checkUserSettings = async ctx => {
-	const prefix  = configuration.prefixUrl('/userSettings');
-	const ppath   = ctx.request.path.substr(0,prefix.length).trim();
-	const sprefix = prefix+'/';
-	if((prefix !== ppath) && (sprefix !== ppath)){return false;}
-	const user    = await fesession.getUser(ctx);
-	if(user === undefined){return false;}
+export const checkUserSettings = async context => {
+	const prefix = configuration.prefixUrl("/userSettings");
+	const ppath = context.request.path.substr(0, prefix.length).trim();
+	const sprefix = prefix + "/";
+	if (prefix !== ppath && sprefix !== ppath) {
+		return false;
+	}
+	const user = await frontEndSession.getUser(context);
+	if (!user) {
+		return false;
+	}
 	return user.passwordExpired !== 1;
 };
-
-const reqStartpage = async ctx => {
-	switch(configuration.get("startpage")){
+const requestHomePage = async context => {
+	switch (configuration.get("startpage")) {
 	case "shop":
-		return await shop.reqGetShop(ctx);
+		return await shop.requestGetShop(context);
 	default:
 	case "login":
-		return await reqLogin(ctx);
+		return await reqLogin(context);
 	}
 };
-
-export const reqFilter = async (ctx,next) => {
-	if((ctx.request.path === '/') || (ctx.request.path === '')){
-		return reqStartpage(ctx,next);
-	}else if(await fesession.checkPassword(ctx)){
-		ctx.redirect(ctx.request.url);
+export const reqFilter = async (context, next) => {
+	if ((context.request.path === "/") || (context.request.path === "")) {
+		return requestHomePage(context, next);
 	}
-	const ses = fesession.get(ctx);
-	if(ses){
-		return next(ctx);
-	}else{
-		return reqLogin(ctx);
+	else if (await frontEndSession.checkPassword(context)) {
+		context.redirect(context.request.url);
+	}
+	const session = frontEndSession.get(context);
+	if (session) {
+		return next(context);
+	}
+	else{
+		return reqLogin(context);
 	}
 };
-
-export const reqRelaxedFilter = async (ctx,next) => {
-	if(fesession.check(ctx) && await pwreset.changeCheck(ctx)){
-		return pwreset.changePage(ctx);
+export const reqRelaxedFilter = async (context, next) => {
+	if (frontEndSession.check(context) && await pwreset.changeCheck(context)) {
+		return pwreset.changePage(context);
 	}
-	return next(ctx);
+	return next(context);
 };
-
-const reqRegister = async ctx => {
-	const arr    = {};
-	arr.title  = 'Registrierung';
-	arr.status = 'Bitte geben Sie ihre E-Mail-Adresse und ein Passwort für Ihr neues Benutzerkonto ein.';
-	arr.errors = [];
-	arr.redirect = '';
-	if((ctx.method === 'POST') && (ctx.request.body !== undefined)){
-		if(fesession.get(ctx)                !== null)     {arr.errors.push("Bitte melden Sie sich ab bevor Sie einen neues Konto anlegen.");}
-		if(ctx.request.body.email            === undefined){arr.errors.push('Bitte geben Sie Ihre E-Mail-Adresse ein');}
-		if(ctx.request.body.password         === undefined){arr.errors.push('Bitte geben Sie ein Passwort');}
-		const email = ctx.request.body.email;
-		const pw    = ctx.request.body.password;
-		if(pw.length < 8){arr.errors.push('Ihr Passwort muss mindestens 8 Zeichen lang sein.');}
+const reqRegister = async context => {
+	const values = {};
+	values.errors = [];
+	values.redirect = "";
+	values.status = "Bitte geben Sie ihre E-Mail-Adresse und ein Passwort für Ihr neues Benutzerkonto ein.";
+	values.title = "Registrierung";
+	const session = frontEndSession.get(context);
+	if (session) {
+		/* Registering a new account while logged in should not be possible. */
+		context.redirect("/");
+		return;
+	}
+	if (context.method === "POST" && context.request.body) {
+		if (!context.request.body.email) {
+			values.errors.push("Bitte geben Sie Ihre E-Mail-Adresse ein.");
+		}
+		if (!context.request.body.password) {
+			values.errors.push("Bitte geben Sie ein Passwort ein.");
+		}
+		const email = context.request.body.email;
+		const password = context.request.body.password;
+		if (password.length < 8) {
+			values.errors.push("Ihr Passwort muss mindestens acht Zeichen lang sein.");
+		}
 		const user = await feuser.getByName(email);
-		if(user !== undefined) {arr.errors.push("Die von Ihnen angegebene E-Mail-Adresse befindet sich bereits im System, bitte verwenden Sie eine andere oder melden Sie sich an.");}
-		if(arr.errors.length === 0){
-			const userid = await feuser.add(email,email,pw);
-			await fesession.start(ctx,userid);
+		if (user) {
+			values.errors.push("Die von Ihnen angegebene E-Mail-Adresse befindet sich bereits im System, bitte verwenden Sie eine andere oder melden Sie sich an.");
+		}
+		if (!values.errors.length) {
+			const userID = await feuser.add(email, email, password);
+			await frontEndSession.start(context, userID);
+			context.redirect("/");
 		}
 	}
-	ctx.body = await template.renderPage('register',arr,false,ctx);
+	context.body = await template.renderPage("register", values, false, context);
 };
-
-const reqGetUserSettings = async ctx => {
-	const user   = await fesession.getUser(ctx);
-	const arr    = {};
-	arr.title  = 'Ihre Daten';
-	arr.status = '';
-	arr.user   = user;
-	arr.user.products = await feuser.getActiveProducts(user.ID);
-	if(ctx.feuserStatus !== undefined){arr.status = ctx.feuserStatus;}
-
-	ctx.body = await template.renderPage('feuserSettings',arr,false,ctx);
+const reqGetUserSettings = async context => {
+	const user = await frontEndSession.getUser(context);
+	const values = {};
+	values.title = "Ihre Daten";
+	values.status = "";
+	values.user = user;
+	values.user.products = await feuser.getActiveProducts(user.ID);
+	if (context.feuserStatus !== undefined) {values.status = context.feuserStatus;}
+	context.body = await template.renderPage("feuserSettings", values, false, context);
 };
-
-const reqPostUserSettings = async ctx => {
-	const user   = await fesession.getUser(ctx);
-	switch(ctx.request.body.verb){
+const reqPostUserSettings = async context => {
+	const user = await frontEndSession.getUser(context);
+	switch (context.request.body.verb) {
 	default:
-		console.warn(`Unknown user settings verb: ${ctx.request.body.verb}`);
+		console.warn(`Unknown user settings verb: ${context.request.body.verb}`);
 		break;
-	case 'pwExpire':
-		if(await feuser.tryLogin(user.name,ctx.request.body.password) !== null){
+	case "pwExpire":
+		if (await feuser.tryLogin(user.name, context.request.body.password) !== null) {
 			feuser.expirePW(user.ID);
-			ctx.redirect(ctx.request.originalUrl);
+			context.redirect(context.request.originalUrl);
 			return null;
 		}
-		ctx.feuserStatus = '<p class="error">Passwort nicht korrekt, bitte &uuml;berpr&uuml;fen Sie ihre Eingabe.</p>';
+		context.feuserStatus = `<p class="error">Passwort nicht korrekt, bitte &uuml;berpr&uuml;fen Sie ihre Eingabe.</p>`;
 		break;
-	case 'deleteUser':
-		ctx.feuserStatus = '<p>Eine E-Mail mit einem Link zum l&ouml;schen all ihrer Daten wurde ihnen soeben gesendet.</p>';
+	case "deleteUser":
+		context.feuserStatus = "<p>Eine E-Mail mit einem Link zum l&ouml;schen all ihrer Daten wurde ihnen soeben gesendet.</p>";
 		addDeletionRequest(user.ID);
 		break;
 	}
-	return await reqGetUserSettings(ctx);
+	return await reqGetUserSettings(context);
 };
-
-const reqUserDeletePage = async ctx => {
-	const arr  = {};
-	const hash = ctx.params.hash;
-	const user = await fesession.getUser(ctx);
-	arr.title  = 'Alle Daten löschen';
-	arr.status = '<p>Bitte pr&uuml;fen Sie den Link und stellen Sie sicher, dass Sie eingeloggt sind damit wir Ihren Account vollst&auml;ndig l&ouml;schen k&ouml;nnen.</p>';
-
-	if((resetRequests[hash] !== undefined) && (resetRequests[hash] === user.ID)){
-		if((ctx.method === 'POST') && (ctx.request.body !== undefined) && (ctx.request.body.confirm !== undefined)){
-			arr.status   = '<p>Ihr Account wurde vollst&auml;ndig und unwiederruflich gel&ouml;scht, vielen Dank dass Sie das mBook genutzt haben.</p>';
+const reqUserDeletePage = async context => {
+	const values = {};
+	const hash = context.params.hash;
+	const user = await frontEndSession.getUser(context);
+	values.title = "Alle Daten löschen";
+	values.status = "<p>Bitte pr&uuml;fen Sie den Link und stellen Sie sicher, dass Sie angemeldet sind, damit wir Ihr Konto vollst&auml;ndig l&ouml;schen k&ouml;nnen.</p>";
+	if (resetRequests[hash] !== undefined && resetRequests[hash] === user.ID) {
+		if (context.method === "POST" && context.request.body !== undefined && context.request.body.confirm !== undefined) {
+			values.status = "<p>Ihr Konto wurde vollst&auml;ndig und unwiederruflich gel&ouml;scht, vielen Dank dass Sie das mBook genutzt haben.</p>";
 			feuser.deleteUser(user.ID);
 			delete resetRequests[hash];
-			fesession.stop(ctx);
-		}else{
-			arr.status = '<p>Sind Sie sicher, dass Sie Ihren Account und alle dazugeh&ouml;rigen Daten l&ouml;schen wollen?</p><form method="post" action="'+ctx.request.originalUrl+'" class="login-form"><input type="submit" class=delete name=confirm value="Account Löschen"/></form>';
+			frontEndSession.stop(context);
+		}
+		else {
+			values.status = `
+				<p>Sind Sie sicher, dass Sie Ihr Konto und alle dazugeh&ouml;rigen Daten l&ouml;schen wollen?</p>
+				<form method="post" action="${context.request.originalUrl}" class="login-form">
+					<input type="submit" class=delete name=confirm value="Konto Löschen"/>
+				</form>
+			`;
 		}
 	}
-	ctx.body = await template.renderPage('userDelete',arr,false,ctx);
+	context.body = await template.renderPage("userDelete", values, false, context);
 };
-
-const reqGetImpressum = async ctx => {
-	const arr = {};
-	arr.title = 'Impressum';
-	ctx.body  = await template.renderPage('impressum',arr,false,ctx);
+const reqGetImpressum = async context => {
+	const values = {};
+	values.title = "Impressum";
+	context.body = await template.renderPage("impressum", values, false, context);
 };
-
-const reqGetDatenschutz = async ctx => {
-	const arr = {};
-	arr.title = 'Datenschutzerklärung';
-	ctx.body  = await template.renderPage('datenschutz',arr,false,ctx);
+const reqGetDatenschutz = async context => {
+	const values = {};
+	values.title = "Datenschutzerklärung";
+	context.body = await template.renderPage("datenschutz", values, false, context);
 };
-
 export const addRoutes = router => {
-	router.post('/userSettings/',reqFilter, reqPostUserSettings);
-	router.post('/userSettings', reqFilter, reqPostUserSettings);
-	router.get ('/userSettings/',reqFilter, reqGetUserSettings);
-	router.get ('/userSettings', reqFilter, reqGetUserSettings);
-	router.all ('/userDelete/:hash', reqFilter, reqUserDeletePage);
-	router.all ('/login',        reqLogin);
-	router.all ('/register',     reqRegister);
-	router.get ('/impressum',    reqGetImpressum);
-	router.get ('/datenschutz',  reqGetDatenschutz);
+	router.post("/userSettings/", reqFilter, reqPostUserSettings);
+	router.post("/userSettings", reqFilter, reqPostUserSettings);
+	router.get("/userSettings/", reqFilter, reqGetUserSettings);
+	router.get("/userSettings", reqFilter, reqGetUserSettings);
+	router.all("/userDelete/:hash", reqFilter, reqUserDeletePage);
+	router.all("/login", reqLogin);
+	router.all("/register", reqRegister);
+	router.get("/impressum", reqGetImpressum);
+	router.get("/datenschutz", reqGetDatenschutz);
 };
--- modules/shop/shop.mjs
+++ modules/shop/shop.mjs
@@ -1,7 +1,5 @@
 import * as cart from "./cart.mjs";
-import * as configuration from "./configuration.mjs";
-import * as feuser from "./feuser.mjs";
-import * as fesession from "./fesession.mjs";
+import * as frontEndSession from "./fesession.mjs";
 import * as order from "./order.mjs";
 import * as template from "./template.mjs";
 import { database } from "./database.mjs";
@@ -30,55 +28,36 @@ export default async () => {
 		);
 	`);
 };
-
-export const reqGetShop = async context => {
-	const arr = {};
-	arr.title = "Shop";
-	context.body = await template.renderPage("shop",arr,true,context);
+export const requestGetShop = async context => {
+	const values = {};
+	values.title = "Shop";
+	context.body = await template.renderPage("shop", values, true, context);
 };
-
-const reqPostShopCheckout = async context => {
-	const arr = {};
-	arr.title = "Warenkorb";
-	context.body = await template.renderPage("checkout",arr,true,context);
+const requestGetOrder = async context => {
+	const values = {
+		frontEndUser: frontEndSession.getUser(context)
+	};
+	values.title = "Warenkorb";
+	context.body = await template.renderPage("checkout", values, true, context);
 };
-
-const tryCreateUser = context => {
-	if (context.method !== "POST" || !context.request.body?.email) {
-		return false;
-	}
-	const email = String(context.request.body.email);
-	return feuser.add(email, email);
-};
-
-const reqPostShopOrder = async context => {
+const requestPostOrder = async context => {
 	const products = cart.get(context);
-	if (fesession.getUser(context) === undefined) {
-		const newUser = await tryCreateUser(context);
-		if (!newUser) {
-			context.body = await template.renderPage("order", {
-				err: "Die E-Mail ist bereits im System vorhanden.",
-				title: "Ein Fehler ist aufgetreten!"
-			}, true, context);
-			return;
-		}
-		await fesession.start(context,newUser);
-		const targetURL = configuration.absoluteUrl("/checkout");
-		context.redirect(targetURL);
-		return;
-	}
-
-	const user = await fesession.getUser(context);
-	if (!order.add({
-		email: user.email,
-		fe_user_id: user.ID
-	}, products)) {
-		context.body = await template.renderPage("order", {
-			err: "Bitte überprüfen Sie Ihre Bestellung.",
-			title: "Ein Fehler ist aufgetreten!"
-		}, true, context);
-		return;
-	}
+	// 	if (!frontEndSession.getUser(context)) {
+	// 		const newUser = await tryCreateUser(context);
+	// 		if (!newUser) {
+	// 			context.body = await template.renderPage("order", {
+	// 				err: "Die E-Mail ist bereits im System vorhanden.",
+	// 				title: "Ein Fehler ist aufgetreten!"
+	// 			}, true, context);
+	// 			return;
+	// 		}
+	// 		await frontEndSession.start(context, newUser);
+	// 		const targetURL = configuration.absoluteUrl("/checkout");
+	// 		context.redirect(targetURL);
+	// 		return;
+	// 	}
+	const user = await frontEndSession.getUser(context);
+	await order.add(user, products);
 	const payPalOrder = await makePayPalOrder(products);
 	context.body = await template.renderPage("payment", {
 		payPalClientID: process.env.PAYPAL_CLIENT_ID,
@@ -86,14 +65,14 @@ const reqPostShopOrder = async context => {
 		title: "Bitte bezahlen Sie Ihre Bestellung."
 	}, true, context);
 };
-const captureCheckout = async context => {
-	const user = await fesession.getUser(context);
+const requestCaptureCheckout = async context => {
+	const user = await frontEndSession.getUser(context);
 	const orderID = await order.getNewestIDByUserID(user.ID);
 	const payPalOrderID = context.headers["x-paypal-order-id"];
 	await capturePayPalOrder(orderID, payPalOrderID);
 	context.body = "";
 };
-const renderOrderDone = async context => {
+const requestOrderDone = async context => {
 	const products = cart.get(context);
 	await cart.empty();
 	context.body = await template.renderPage("order-done", {
@@ -102,11 +81,11 @@ const renderOrderDone = async context => {
 	}, true, context);
 };
 export const addRoutes = router => {
-	router.get("/", reqGetShop);
-	router.get("/order", reqPostShopCheckout);
-	router.get("/order-done", renderOrderDone);
-	router.get("/checkout", reqPostShopCheckout);
-	router.post("/checkout", reqPostShopCheckout);
-	router.post("/checkout/capture", captureCheckout);
-	router.post("/order", reqPostShopOrder);
+	router.get("/", requestGetShop);
+	router.get("/order", requestGetOrder);
+	router.get("/order-done", requestOrderDone);
+	router.get("/checkout", requestGetOrder);
+	router.post("/checkout", requestGetOrder);
+	router.post("/checkout/capture", requestCaptureCheckout);
+	router.post("/order", requestPostOrder);
 };
