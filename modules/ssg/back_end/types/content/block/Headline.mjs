a47d085aaa78af3771b6d525470052794685f950
Benjamin Vincent Schulenburg <bennyschulenburg@gmx.de>
2021-07-30 15:34:37 +0200

    Headline text is now HTML escaped
    Former-commit-id: 5fcb0788da5a441b1d3a27710b51f3564eb81ea3

--- modules/ssg/back_end/types/content/block/Headline.mjs
+++ modules/ssg/back_end/types/content/block/Headline.mjs
@@ -37,24 +37,18 @@ export default {
 		tag = "h3"
 	}, {
 		contentTypeIDIf,
-		EditorialError,
+		escapeHTML,
 		helpers: {
 			Marker
 		}
 	}) {
-		const embeddedTagError = String(headline).trim().startsWith("<h")
-			? EditorialError.render({
-				message: "A Headline should only use HTML header tags (h1 to h6), please change the content accordingly, thank you."
-			})
-			: "";
 		const hTag = ["h1","h2","h3","h4","h5","h6"].includes(tag) ? tag : "h3";
 		const isReallyNumbered = isNumbered && tag === "h1";
 		return `
 			<section content-type="headline" ${contentTypeIDIf(id)}>
 				<inner-content>
 					${Marker.render({ isNumbered: isReallyNumbered })}
-					${embeddedTagError}
-					<${hTag}>${headline}</${hTag}>
+					<${hTag}>${escapeHTML(headline)}</${hTag}>
 				</inner-content>
 			</section>
 		`;
