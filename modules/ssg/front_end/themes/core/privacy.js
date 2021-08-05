14b7607a98b3f8727ea4cedfdd9327f35a44f512
Tobias Pätzold <tobias.paetzold@institut-fuer-digitales-lernen.de>
2021-08-05 12:18:39 +0200

    basic styling of privacy modal
    Former-commit-id: efa9fee4a7aae1410c9f371192fa37bd57204ec0

--- modules/ssg/front_end/themes/core/privacy.js
+++ modules/ssg/front_end/themes/core/privacy.js
@@ -15,9 +15,10 @@ const canUseEmbeds = () => new Promise(resolve => {
 
 	const modal = showModal(`<p>Fuer externe Embeds, benoetigen wir Ihre Zustimmung, eine Verbindung zu dem jeweiligem Anbieter aufzubauen</p>
 		<p>In unserer <a href="./datenschutz/" target="_blank">Datenschutzerklaerung</a> koennen Sie mehr dazu erfahren</p>
-
+		<div id="privacy-btn-wrap">
 		<button button-type="accept">Akzeptieren</button>
-		<button button-type="cancel">Abbrechen</button>`);
+		<button button-type="cancel">Abbrechen</button>
+		</div>`);
 
 	modal.querySelector(`button[button-type="accept"]`).addEventListener("click", async () => {
 		await allowUseOfEmbeds(true);
