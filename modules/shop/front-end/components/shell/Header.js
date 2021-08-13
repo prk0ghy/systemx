fe16386f4a8a90747d2d9a3d471d8e9777e0d1d1
Tobias Pätzold <tobias.paetzold@institut-fuer-digitales-lernen.de>
2021-08-13 18:23:24 +0200

    added headerbar scroll transition
    Former-commit-id: 707f6d9347ec7718353ca49c90529e82a5a7fbda

--- modules/shop/front-end/components/shell/Header.js
+++ modules/shop/front-end/components/shell/Header.js
@@ -1,11 +1,37 @@
+import {
+	useCallback,
+	useEffect,
+	useState
+} from "react";
+import cx from "classnames";
 import Laced from "components/generics/Laced";
 import Navigation from "components/shell/Navigation";
 import styles from "./Header.module.css";
-const Header = () => (
-	<header className={ styles.header }>
-		<Laced>
-			<Navigation className={ styles.navigation }/>
-		</Laced>
-	</header>
-);
+const Header = () => {
+	const [isExpanded, setIsExpanded] = useState(false);
+	const handleScroll = useCallback(() => {
+		const limitY = 240;
+		setIsExpanded(window.scrollY >= limitY);
+	}, []);
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll, {
+			passive: true
+		});
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, [
+		handleScroll
+	]);
+	const headerClassName = cx(styles.header, {
+		[styles.expanded]: isExpanded
+	});
+	return (
+		<header className={ headerClassName }>
+			<Laced>
+				<Navigation className={ styles.navigation }/>
+			</Laced>
+		</header>
+	);
+};
 export default Header;
