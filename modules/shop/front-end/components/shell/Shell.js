c7ada43193530660f42ce4366b641e75168e4687
Kai Riemann <kai@riemann.dev>
2021-08-27 20:31:58 +0200

    Translated entire shell when side menu opens
    Former-commit-id: 0230dd6dc05203e60ef9f311430d98dc852f9662

--- modules/shop/front-end/components/shell/Shell.js
+++ modules/shop/front-end/components/shell/Shell.js
@@ -1,24 +1,39 @@
 import { BrandProvider } from "contexts/Brand";
 import { BusProvider } from "contexts/Bus";
 import { CartProvider } from "contexts/Cart";
+import cx from "classnames";
 import Footer from "components/shell/Footer";
 import Header from "components/shell/Header";
 import { ProductsProvider } from "contexts/Products";
 import SideMenu from "components/shell/SideMenu";
 import styles from "./Shell.module.css";
+import { useBus } from "contexts/Bus";
+const ShellContent = ({ children }) => {
+	const [{ isSideMenuOpen }] = useBus();
+	const shellContentClassName = cx(styles.content, {
+		[styles.inactive]: isSideMenuOpen
+	});
+	return (
+		<div className={ styles.shell }>
+			<div className={ shellContentClassName }>
+				<Header/>
+				<main className={ styles.children }>
+					{ children }
+				</main>
+				<Footer/>
+			</div>
+			<SideMenu/>
+		</div>
+	);
+};
 const Shell = ({ children }) => (
 	<BrandProvider>
 		<ProductsProvider>
 			<CartProvider>
 				<BusProvider>
-					<div className={ styles.shell }>
-						<Header/>
-						<main className={ styles.content }>
-							{ children }
-						</main>
-						<Footer/>
-					</div>
-					<SideMenu/>
+					<ShellContent>
+						{ children }
+					</ShellContent>
 				</BusProvider>
 			</CartProvider>
 		</ProductsProvider>
