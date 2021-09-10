58d16d79b095f4d993a3f99275bfadafb37e20a1
Benjamin Vincent Schulenburg <bennyschulenburg@gmx.de>
2021-09-10 12:42:16 +0200

    Fixed sqlite init error
    Former-commit-id: 15b984a12fdab0dd7df16ec25b288fe44833f337

--- modules/shop/back_end/database.mjs
+++ modules/shop/back_end/database.mjs
@@ -1,7 +1,10 @@
 import sqlite3 from "sqlite3";
 import { open } from "sqlite";
+import { mkdirp } from "../../common/fileSystem.mjs";
 import options from "../../common/options.mjs";
 import path from "path";
+
+await mkdirp(options.storagePath);
 
 const database = await open({
 	driver: sqlite3.Database,
