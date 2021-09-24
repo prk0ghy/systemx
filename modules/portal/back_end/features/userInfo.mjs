5cb48a857cf721be401923cc417275e0104d1967
Benjamin Vincent Schulenburg <bennyschulenburg@gmx.de>
2021-09-24 13:30:18 +0200

    Created userInfo.mjs out of the separate files with a getter and setter

--- modules/portal/back_end/features/userInfo.mjs
+++ modules/portal/back_end/features/userInfo.mjs
@@ -1,6 +1,18 @@
 import Filter from "../filter.mjs";
 import * as User from "../user.mjs";
 
+Filter("userInfoGet",async (v,next) => {
+	const user = await User.getByID(v.ses?.user?.ID|0);
+	delete user?.password;
+	if(!user){
+		v.res.error = "Session not found";
+		return v;
+	}
+	v.res.user = user;
+	return await next(v);
+});
+
+
 Filter("userInfoSet",async (v,next) => {
 	const user = await User.getByID(v.ses?.user?.ID|0);
 	console.log(v);
