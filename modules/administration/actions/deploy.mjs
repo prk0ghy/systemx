f9809aa268b2b00db90998b9ea1c22117d871b19
Benjamin Vincent Schulenburg <bennyschulenburg@gmx.de>
2021-12-07 19:17:29 +0100

    Some final mVet touches

---
+++ modules/administration/actions/deploy.mjs
@@ -0,0 +1,25 @@
+import makeid from "../../common/randomString.mjs";
+import { jobs } from "../job.mjs";
+
+const asyncTimeout = (ms) => new Promise((resolve) => {
+    setTimeout(() => resolve() , ms);
+});
+
+const fakeProgress = async (jid) => {
+    for(let i = 0; i < 100; i++){
+        await asyncTimeout(1000);
+        jobs[jid].status = "running";
+        jobs[jid].progress = i + 0.0;
+    }
+    jobs[jid].status = "complete";
+    jobs[jid].progress = 100.0;
+}
+
+const jobDeploy = (data) => {
+    const jid = makeid(64);
+    if(jobs[jid]){return jobDeploy(data);}
+    jobs[jid] = {...data, status: "started", id: jid, progress: 0.0};
+    fakeProgress(jid);
+    return jobs[jid];
+};
+export default jobDeploy;
