1df500af67d1c7626d4e813e138c1cf3534fe00e
Benjamin Vincent Schulenburg <bennyschulenburg@gmx.de>
2022-02-03 16:25:26 +0100

    Removed ImageText outline

--- modules/userLogin/front_end/components/generics/ImageText.js
+++ modules/userLogin/front_end/components/generics/ImageText.js
@@ -1,9 +1,8 @@
 import cx from "classnames";
-import styles from "./ImageText.module.css";
 const ImageText = ({
 	children,
 	className
 }) => (
-	<div className={ cx(styles.imageText, className) }>{ children }</div>
+	<div className={ cx(className) }>{ children }</div>
 );
 export default ImageText;
