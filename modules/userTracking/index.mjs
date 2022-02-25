f5aba8562a3e4691ad6df931de69fd80b2cb0eec
Benjamin Vincent Schulenburg <bennyschulenburg@gmx.de>
2022-02-25 11:31:58 +0100

    This should not produce a warning

--- modules/userTracking/index.mjs
+++ modules/userTracking/index.mjs
@@ -3,6 +3,6 @@ import serve from "./server.mjs";
 
 const start = async () => {
 	serve(currentTarget);
-	console.log(`UserTracking started: http://localhost:${options.httpPort}/`)
+	console.log(`UserTracking started: http://localhost:${options.httpPort}/`);
 };
 export default start;
