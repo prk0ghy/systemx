ca5a36cb6ddbbed2f06235bb8185b794e020d7be
Stefan Wintergerst <stefan.wintergerst@team-parallax.com>
2022-03-11 12:09:45 +0100

    Add star component (#2)
    Co-authored-by: Benjamin Vincent Schulenburg <bennyschulenburg@gmx.de>

---
+++ modules/contentPipeline/front_end/content_types/star-selection/star-selection.js
@@ -0,0 +1,276 @@
+/* globals scrollToElement */
+(() => {
+	const C_PROPERTY_KEY = "star-selection";
+	const C_FOR_SECTION_KEY = "for-star";
+	/**
+ 	 * The top-offset used to scroll to star sections
+     */
+	const C_SCROLL_OFFSET = 150;
+	let sectionIdCounter = 0;
+	/** @type {Element[]} */
+	let sections = [];
+	const initStarSelection = () => {
+		sections = Array.from(document.querySelectorAll("main>section"));
+		const sectionsWithStars = [];
+		// find all sections without the property 'star-selection'
+		// who have sibling elements with the property
+		for (const section of sections) {
+			if (section.hasAttribute(C_PROPERTY_KEY)) {
+				continue;
+			}
+			let nextSib = section.nextElementSibling;
+			const starElements = [];
+			// find siblings star element sibling
+			while(nextSib !== null && nextSib.hasAttribute(C_PROPERTY_KEY)) {
+				starElements.push(nextSib);
+				nextSib = nextSib.nextElementSibling;
+			}
+			if (starElements.length > 0) {
+				sectionsWithStars.push({
+					section,
+					starElements
+				});
+			}
+		}
+
+		for(const {section, starElements} of sectionsWithStars) {
+			// mark section to have star siblings
+			section.setAttribute("with-stars", starElements.length);
+			section.id = sectionIdCounter++;
+			// remove markers from star elements
+			for (const starElement of starElements) {
+				const markers = Array.from(starElement.querySelectorAll("inner-content > a.marker"));
+				const marker = markers.pop();
+				if (marker) {
+					marker.remove();
+				}
+				// mark the section as a star element sibling for the parent section
+				starElement.setAttribute(C_FOR_SECTION_KEY, section.id);
+			}
+
+			const hasHelpVideo = section.querySelectorAll("inner-content > help-video").length > 0;
+			const starButton = createStarButton();
+			const container = document.createElement("div");
+			container.classList.add("star-button-container");
+			if (hasHelpVideo) {
+				container.classList.add("offset");
+			}
+			container.appendChild(starButton);
+			// account for various content paddings/margins
+			const absHeight = section.offsetHeight + 64 + 25 + (isMobile() ? -96 : 0);
+			const starButtons = createStarButtons(starElements, absHeight, section);
+			starButtons.forEach(s => {
+				container.appendChild(s);
+			});
+			let isOpen = false;
+			setVisibility(section, "visible");
+			starButton.onclick = () => {
+				isOpen = !isOpen;
+				starButtons.forEach(s => {
+					s.classList.toggle("shift-in");
+					setVisibility(s, isOpen ? "visible" : "hidden");
+				});
+				if (!isOpen) {
+					resetStarElements(starElements);
+					// if the section is hidden, add a fade animation
+					if (!isVisible(section)) {
+						section.classList.add("fadein");
+					}
+					setVisibility(section, "visible");
+					starButton.classList.remove("open");
+					unblurAllSections();
+				} else {
+					starButton.classList.add("open");
+					section.classList.remove("unblur");
+					resetStarElements(starElements);
+				}
+				if (isOpen) {
+					starButton.classList.add("open");
+				} else {
+					starButton.classList.remove("open");
+				}
+			};
+			section.firstElementChild.appendChild(container);
+		}
+	};
+
+	setTimeout(initStarSelection, 0);
+
+	/**
+	 *
+	 * @returns {HTMLAnchorElement}
+	 */
+	const createStarButton = () => {
+		const icon = document.createElement("a");
+		icon.classList.add("bubble", "star");
+		const img = document.createElement("img");
+		img.setAttribute("title", "Zusatzinfos");
+		icon.appendChild(img);
+		return icon;
+	};
+
+	/**
+	 * @param {string} icon
+	 * @param {string} title
+	 * @param {string} title
+	 * @returns
+	 */
+	const getImage = (icon, title) => `<img title="${title}" src="/resources/svg/star-${icon}.svg" />`;
+
+	/**
+	 * @param {Element[]} starElements
+	 * @param {number} offset
+	 * @param {Element} section
+	 * @returns {Element[]}
+	 */
+	const createStarButtons = (starElements, offset, section) => {
+		const isLastSection = starElements[starElements.length - 1].nextElementSibling === null;
+		return starElements.map(starElement => {
+			const a =  document.createElement("a");
+			const ct = starElement.getAttribute("content-type");
+			a.setAttribute("content-type", ct);
+			a.classList.add("bubble");
+			a.style.visibility = "hidden";
+			const {icon, title } = getIconFromContentType(isImage(starElement) ? "image" : ct);
+			a.innerHTML = getImage(icon, title);
+			a.setAttribute("title", title);
+			a.onclick = () => {
+				// reset all other star elements
+				starElements.filter(se => !se.isSameNode(starElement)).forEach(se => {
+					se.style.display = null;
+					se.style.top = null;
+				});
+				if (starElement.style.display === "block") {
+					return;
+				} else {
+					starElement.classList.add("fadein");
+					blurSections(section.id);
+					starElement.style.display = "block";
+					// if the parent section is the last one we need to offset it slightly less
+					// for some reason
+					starElement.style.top = `-${!isLastSection ? offset : offset - 25}px`;
+					// add bottom margin if the element is smaller than the original section
+					const computedOffset = offset - (starElement.offsetHeight > offset ? 0 : starElement.offsetHeight) - 100;
+					starElement.style.marginBottom = `-${computedOffset}px`;
+					setVisibility(section, "hidden");
+					scrollToElement(section, C_SCROLL_OFFSET);
+				}
+			};
+			return a;
+		});
+	};
+
+	/**
+	 * @param {Element} element
+	 * @param {"visible" | "hidden"} visibility
+	 */
+	const setVisibility  = (element, visibility) => {
+		element.style.visibility = visibility;
+	};
+
+	/**
+	 * @param {Element} element
+	 * @returns
+	 */
+	const isVisible = (element) => element.style.visibility === "visible";
+
+	/**
+	 * @param {Element[]} starElements
+	 */
+	const resetStarElements = (starElements) => {
+		starElements.forEach(starElement => {
+			starElement.style.display = null;
+			starElement.style.top = null;
+			starElement.classList.remove("unblur", "blur");
+		});
+	};
+
+	/**
+	 * Blur all sections except the section with the given id
+	 * and sections which are star-element siblings for the given id
+	 * @param {string} exceptId
+	 */
+	const blurSections = (exceptId) => {
+		sections
+			.filter(s => s.id !== exceptId)
+			.filter(s => s.getAttribute(C_FOR_SECTION_KEY) !== exceptId)
+			.forEach(s => {
+				s.classList.add("blur");
+			});
+	};
+
+	/**
+	 * un-blur all sections.
+	 */
+	const unblurAllSections = () => {
+		sections
+			.filter(s => s.classList.contains("blur"))
+			.forEach(s => {
+				s.classList.remove("blur");
+				s.classList.add("unblur");
+			});
+	};
+
+
+	/**
+	 * Determine if a 'text-and-image' section should use the image or text icon.
+	 * If the section contains a div with the 'text-content', text icon will be used.
+	 * If doens't contain text and contains figures or figure-rows it will use the
+	 * image icon.
+	 * @param {Element} element
+	 * @returns true if the element should use the image icon, false otherwise.
+	 */
+	const isImage = (element) => {
+		const ct = element.getAttribute("content-type");
+		if (ct !== "text-image") {
+			return false;
+		}
+		const hasText = element.querySelectorAll("inner-content > .text-content").length > 0;
+		if (hasText) {
+			return true;
+		}
+		const hasFigures = element.querySelectorAll("inner-content > figure").length > 0;
+		const hasFigureRows = element.querySelectorAll("inner-content > figure-row").length > 0;
+		const hasImages = hasFigureRows || hasFigures;
+		return hasImages;
+	};
+
+	/**
+	 * @param {string} contentType
+	 * @returns
+	 */
+	const getIconFromContentType = (contentType) => {
+		let title = "";
+		let icon = "";
+		switch(contentType) {
+		case "text-and-image" :
+			icon = "text";
+			title = "Text / Bild";
+			break;
+		case "embedding":
+			icon = "video";
+			title = "Video";
+			break;
+		case "audio":
+			icon = "audio";
+			title = "Audio";
+			break;
+		case "image": {
+			icon = "image";
+			title = "Bild(er)";
+			break;
+		}
+		default:
+			icon = "??";
+			break;
+		}
+		return {
+			icon,
+			title
+		};
+	};
+
+	const isMobile = () => {
+		return window.innerWidth <= 820;
+	};
+})();
