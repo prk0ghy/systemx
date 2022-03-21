78a1c0d3a6e2abcc3326ee62a1be8da40663a9b5
David Messingschlager <dm@kupfersync.systems>
2022-03-21 16:36:07 +0100

    Work on html to text

--- scripts/htmlMailToText.mjs
+++ scripts/htmlMailToText.mjs
@@ -2,23 +2,31 @@
 * This script will parse any HTML file in a specific folder and create a corresponding .txt file
 * https://www.npmjs.com/package/htmlparser2
 */
+import { rejects } from "assert";
 import fs from "fs";
 import htmlparser2 from "htmlparser2";
+import { resolve } from "path";
 
 let mailText = "";
 const parserStream = new htmlparser2.Parser({
 	ontext(text) {
-		//if (text != "") {
-		mailText += text;
-		//}
-	}, onclosetag() {
+		if (text.trim() != "") {
+			if (text.slice(0,-1) =="ö" ||text.slice(0,-1) =="ä" || text.slice(0,-1) =="ü") {
+				mailText += text.trim();
+			}
+			else {
+				mailText += "\n" + text.trim();
+			}
+		}
+	},
+	onclosetag() {
 		write_emailtext();
 	}
 });
 
-fs.readFile("/var/www/html/systemx/modules/userLogin/back_end/templates/passwordResetMail.html", (err, data) => {
+fs.readFile("/var/www/html/systemx/modules/userLogin/back_end/templates/passwordResetMail.html", (err,data) => {
 	if (err) {
-		throw err;
+		return;
 	}
 	parserStream.write(data);
 	parserStream.end();
@@ -28,11 +36,12 @@ console.log(mailText);
 
 
 function write_emailtext() {
-	fs.writeFile("/var/www/html/systemx/modules/userLogin/back_end/templates/passwordResetMail.txt", mailText, err => {
+	fs.writeFile(("/var/www/html/systemx/modules/userLogin/back_end/templates/" + "passwordResetMail.txt"), mailText , err => {
 		if (err) {
-			throw err;
+			rejects(err);
+			return;
 		}
 	})
-	fs.close(0);
-	//fs.closeSync(0);
-}
+
+};
+fs.close(0);
